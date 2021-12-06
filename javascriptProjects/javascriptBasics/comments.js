737c3437e329db6df996559146400b2964bf0ca6
Seth McLaughlin <seth.d.mcLaughlin@gmail.com>
2021-12-06 16:02:22 -0500

    javascript day one

---
+++ javascriptProjects/javascriptBasics/comments.js
@@ -0,0 +1,11 @@
+// This is a single line comment, I am ignored (hopefully not by the developer)
+
+console.log("running javascript..."); // I am still a comment
+
+/*
+
+I am a
+multi-line
+comment.
+
+*/
---
+++ javascriptProjects/javascriptBasics/variables.js
@@ -0,0 +1,18 @@
+// Javascript Variable Declaration
+var dinnerChoice;
+
+// A variable is a named box that holds something in Javascript
+console.log(dinnerChoice);
+
+// Initialize a variable with a new value
+dinnerChoice = "Ramen";
+
+console.log(dinnerChoice);
+
+dinnerChoice = "Popeyes";
+console.log(dinnerChoice);
+
+// Declare and Initialize
+var coffeeType = "Doubleshot Breve no Classic";
+let dya;
+dya = "yo";
